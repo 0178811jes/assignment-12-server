f513b1bf0942bdb27c6b204cbc4a172b0415b7ec
0178811jes <jesminara8653@gmail.com>
2022-11-26 15:50:47 +0600

    jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jsonwebtoken = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,6 +22,7 @@ async function run (){
     try{
         const appointmentOptionCollection=client.db('resellProduct').collection('appointmentOptions');
         const bookingsCollection=client.db('resellProduct').collection('bookings');
+        const usersCollection=client.db('resellProduct').collection('users');
 
         app.get('/appointmentOptions', async(req, res) => {
             const date=req.query.date;
@@ -29,13 +31,77 @@ async function run (){
             const options = await appointmentOptionCollection.find(query).toArray();
             res.send(options);
         });
+
+        //category data load
+        // app.get('/appointmentOptions/category/:id', async(req, res)=>{
+        //     const id = req.params.id;
+        //     const query = {_id: ObjectId(id)};
+        //     const category = await appointmentOptionCollection.find(id);
+        //     res.send(category);
+        // })
+
+        //bookings get
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         //bookings
         app.post('/bookings', async(req, res) => {
             const booking = req.body;
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
+        });
+        //all user data 
+        app.get('/users', async(req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+        //jwt token is
+        app.get('/jwt', async(req, res) => {
+            const email =req.query.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'});
+                return res.send({accessToken: token});
+            }
+            res.status(403).send({accessToken: ''})
         })
 
+        //users posting 
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+
+        });
+
+        //admin user
+        app.put('/users/admin/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = { _id : ObjectId (id) };
+            const options = { upsert: true};
+            const updatedDoc = {
+                $set:{
+                    role: 'admin',
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+
+        });
+
+        //admin user get
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            req.send({isAdmin: user?.role === 'admin'});
+        })
 
 
     }
@@ -48,30 +114,12 @@ run().catch(console.log);
 
 
 
-//client side theke load//
-
-const categories = require('./data/Categories.json');
-const allProducts = require('./data/ProductCategory.json');
 
 app.get('/', (req, res) => {
     res.send('assignment 12 server is running')
 });
 
-app.get('/product-categories', (req, res) => {
-    res.send(categories);
-});
 
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    const categoryProduct = allProducts.filter(product => product.category_id === id);
-    res.send(categoryProduct);
-})
-
-app.get('/product/:id', (req, res) => {
-    const id = req.params.id;
-    const selectProduct = allProducts.find(product => product._id === id);
-    res.send(selectProduct);
-});
 
 app.listen(port, () => {
     console.log(`assignment 12 server running on port ${port}`)
